f1e68f9d4d9ef58d63a4f3c51796c6c4e66c1083
Guilherme Caraciolo <gcaraciolo@gmail.com>
2016-04-02 16:35:29 -0300

    deserializable

---
+++ src/main/java/br/unicap/cardgame/model/Answer.java
@@ -0,0 +1,29 @@
+package br.unicap.cardgame.model;
+
+public class Answer {
+    
+    private int ID;
+    private String text;
+
+    public Answer(int ID, String text) {
+        this.ID = ID;
+        this.text = text;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+       
+}
---
+++ src/main/java/br/unicap/cardgame/model/Question.java
@@ -0,0 +1,52 @@
+package br.unicap.cardgame.model;
+
+import java.util.List;
+
+public class Question {
+ 
+    private int ID;
+    private String text;
+    private List<Answer> possibleAnswers;
+    private Answer correctAnswer;
+
+    public Question(int ID, String text, List<Answer> possibleAnswers, Answer correctAnswer) {
+        this.ID = ID;
+        this.text = text;
+        this.possibleAnswers = possibleAnswers;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Answer> getPossibleAnswers() {
+        return possibleAnswers;
+    }
+
+    public void setPossibleAnswers(List<Answer> possibleAnswers) {
+        this.possibleAnswers = possibleAnswers;
+    }
+
+    public Answer getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(Answer correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+    
+    
+}
